b6b4da490bc766af65fa942f67232d495a9ef564
TestardR <romain.rtestard@gmail.com>
2021-10-24 18:41:51 +0200

    feature(design): add 5 design patterns

---
+++ observor/docs.go
@@ -0,0 +1,9 @@
+package observer
+
+/*
+	1. Observer is an instrusive approach
+	2. Must provide a way of clients to subscribe
+	3. Event data sent from observable to all observers
+	4. Data represented as interface{}
+	5. Unsubscription is possible
+*/
